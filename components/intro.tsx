1ee5a4867124fbc57f7a715bd9e1e6e0e5340022
LAN <1548941823@qq.com>
2025-02-26 17:41:59 +0800

    更新仓库位置

--- components/intro.tsx
+++ components/intro.tsx
@@ -111,9 +111,9 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] 
-          rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 
-          transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/wocgoda/My-first-weebsite" 
+          rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 
+          active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com/wocgoda/My-first-weebsite"  
           target="_blank"
         >
           <FaGithubSquare />
